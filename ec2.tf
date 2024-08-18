e1baa30a11bd1326ced8471ff275c5fd290dc8c8
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-18 19:31:57 +0300

    Add the AMI.id to the ec2

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
   # Create Ec2 Instance called iris_tf_demo_ec2_instance
 **********************************************************/
 resource "aws_instance" "cloudwatch_ec2_instances" {
-  ami           = data.aws_ami.ec2_ami_filter
+  ami           = data.aws_ami.ec2_ami_filter.id
   instance_type = "t2.micro"
   user_data     = file("scripts/entry.sh")
 
