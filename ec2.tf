72b135647246246c2793bc18502432f5b428564f
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-20 00:18:29 +0300

    Remove count

--- ec2.tf
+++ ec2.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "demo_alb_waf_ec2_instance_1" {
   instance_type = "t2.micro"
   user_data     = file("scripts/entry.sh")
 
-  count = 2
-
   tags = {
     Name = "elb_project"
   }
