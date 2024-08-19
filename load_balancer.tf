ca0e69bbfaa3f6d53198aa0e1f1a4aba34942912
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-19 21:04:32 +0300

    Add the load balancer and 2 subnets

--- load_balancer.tf
+++ load_balancer.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "elb_load_balancer" {
   name               = "ec2-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.elb_project_sg.id]
-  subnets            = [aws_subnet.elb_project_subnet.id]
+  security_groups    = [ aws_security_group.elb_project_sg.id ]
+  subnets            = [ aws_subnet.elb_project_subnet_2.id, aws_subnet.elb_project_subnet_1.id ]
 
   enable_deletion_protection = true
 
--- vpc.tf
+++ vpc.tf
@@ -6,9 +6,18 @@ resource "aws_vpc" "elb_project" {
   }
 }
 
-resource "aws_subnet" "elb_project_subnet" {
+resource "aws_subnet" "elb_project_subnet_1" {
   vpc_id     = aws_vpc.elb_project.id
   cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "elb_project"
+  }
+}
+
+resource "aws_subnet" "elb_project_subnet_2" {
+  vpc_id     = aws_vpc.elb_project.id
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "elb_project"
