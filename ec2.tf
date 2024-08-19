df6c7ebec87f2e3dd94d10e0a487eda0f57e8a48
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-08-19 21:43:53 +0300

    New ec2 logic

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,25 @@
 /**********************************************************
   # Create Ec2 Instance called iris_tf_demo_ec2_instance
 **********************************************************/
-resource "aws_instance" "demo_alb_waf_ec2_instance" {
+resource "aws_instance" "demo_alb_waf_ec2_instance_1" {
   ami           = data.aws_ami.ec2_ami_filter.id
   instance_type = "t2.micro"
   user_data     = file("scripts/entry.sh")
 
   count = 2
+
+  tags = {
+    Name = "elb_project"
+  }
+}
+
+/**********************************************************
+  # Create Ec2 Instance called iris_tf_demo_ec2_instance
+**********************************************************/
+resource "aws_instance" "demo_alb_waf_ec2_instance_2" {
+  ami           = data.aws_ami.ec2_ami_filter.id
+  instance_type = "t2.micro"
+  user_data     = file("scripts/entry.sh")
 
   tags = {
     Name = "elb_project"
